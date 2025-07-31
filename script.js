84ff63df5670954e44574b4333c136a40db6edc1
Dea <ddeasimnica@gmail.com>
2025-07-31 21:23:43 +0200

    Add slideshow

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.querySelectorAll('.slideshow').forEach(slideshow => {
+const slides = slideshow.querySelectorAll('.project-slide');
+let currentIndex = 0;
+
+const showSlide = (index) => {
+    slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+    });
+};
+
+const nextButtons = slideshow.querySelectorAll('.next-inline');
+nextButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+    });
+});
+
+showSlide(currentIndex);
+});
+
